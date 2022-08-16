3341759231086ad6309aa9a7268ae919f1165a4f
smoulart <smoulart@osaxis.fr>
2022-08-16 18:54:21 +0200

    Add conference component to app

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConferencesComponent } from './components/conferences/conferences.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'conferences', component:ConferencesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConferencesComponent } from './components/conferences/conferences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConferencesComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
