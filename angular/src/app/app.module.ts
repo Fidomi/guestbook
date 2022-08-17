75101d42a2704eeb038c4a75e4c29747c8707c74
smoulart <smoulart@osaxis.fr>
2022-08-17 14:16:37 +0200

    Change some style

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConferencesComponent } from './components/conferences/conferences.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ConferencesComponent } from './components/conferences/conferences.compo
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
